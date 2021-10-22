aacab0d48763b5b29c03ff51d4844f3983b4103b
prathmesh shinde <shindeprathmesh99@gmail.com>
2021-10-22 21:53:54 +0530

    heyo

--- 7.c
+++ 7.c
@@ -1,1 +1,12 @@
 #include<stdio.h>
+void main(){
+    float r, b, h; // r for radius, b for base of triangle, h for height of triangle
+    float cArea, cCircum, tArea;  // circles area, circumference, triangles area
+    printf("Please give me your radius, base and height\n");
+    scanf("%f %f %f", &r, &b, &h);
+    cArea = 3.14*r*r;
+    cCircum = 2*3.14*r;
+    tArea = 0.5*b*h;
+    printf("Circle Area: %f, Circumference %f\n Triangle Area: %f \n", cArea, cCircum, tArea);
+    
+}
